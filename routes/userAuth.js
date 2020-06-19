658079206573a9efb1ebb7e53ebbd9ce9003f8b0
internabhijit <intern.abhijit@coutloot.com>
2020-06-20 00:29:18 +0530

    remove index

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -4,9 +4,6 @@ let router = express.Router();
 let { login, registration, logout } = require("../controllers/userAuth");
 let { validate } = require("../middleware/validation");
 
-router.get("/", (req, res) => {
-  res.send("Server Is Running");
-});
 router.post("/login", login);
 router.post("/registration", registration);
 router.get("/logout", validate, logout);
